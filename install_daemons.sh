0b3fe0cd1f99bbb30772fd30f7aefe96a1f68e02
Brian Davis <bwdavis11@gmail.com>
2017-01-16 22:26:24 -0800

    final it

--- install_daemons.sh
+++ install_daemons.sh
@@ -4,7 +4,6 @@ git checkout REL9_5_STABLE
 ./configure --prefix=$1
 make
 make install
-cd ~
 curl http://www-eu.apache.org/dist//httpd/httpd-2.4.25.tar.bz2 > httpd-2.4.25.tar.bz2
 tar xvfj httpd-2.4.25.tar.bz2 
 cd httpd-2.4.25.25
