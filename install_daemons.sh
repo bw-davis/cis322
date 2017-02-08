d069c16e18ee073dd75deab8f425f60700187a76
Brian Davis <bwdavis11@gmail.com>
2017-02-08 14:15:23 -0800

    fixed install_daemons issue

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,24 +1,13 @@
-
 cd ~
 git clone https://github.com/postgres/postgres.git
-
-
-
-cd postgres/
+cd postgres
+git checkout REL9_5_STABLE
 ./configure --prefix=$1
 make
 make install
-cd ..
-
-
-
-curl -o httpd-2.4.25.tar.bz2 http://apache.mirrors.tds.net//httpd/httpd-2.4.25.tar.bz2
-
-
-
-tar -xjf httpd-2.4.25.tar.bz2
-cd httpd-2.4.25/
+curl http://www-eu.apache.org/dist//httpd/httpd-2.4.25.tar.bz2 > httpd-2.4.25.tar.bz2
+tar xvfj httpd-2.4.25.tar.bz2 
+cd httpd-2.4.25.25
 ./configure --prefix=$1
 make
 make install
-cd ..
