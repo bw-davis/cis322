006fc95357152db4a26d9d39fa974230525ddf77
Brian Davis <bwdavis11@gmail.com>
2017-02-08 15:17:04 -0800

    for real fixed it this time

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -14,75 +14,72 @@ create table assets (
     description text,
     alt_description text
 );
-create table vehicles {
+create table vehicles (
     vehicle_pk  serial primary key,
-    asset_fk    integer REFERENCES assets(asset_pk) not null,
-};
-create table facilities {
+    asset_fk    integer REFERENCES assets(asset_pk) not null
+);
+create table facilities (
     facility_pk serial primary key,
     fcode   varchar(16),
     common_name varchar(128),
     location    varchar(128)
-};
-create table asset_at {
+);
+create table asset_at (
     asset_fk    integer REFERENCES assets(asset_pk) not null,
     facility_fk integer REFERENCES facilities(facility_pk) not null,
     arrive_dt   timestamp,
     depart_dt   timestamp
-};
-create table convoys {
+);
+create table convoys (
     convoy_pk   serial primary key,
     request varchar(16),
     source_fk   integer REFERENCES facilities(facility_pk) not null,
     dest_fk integer REFERENCES facilities(facility_pk) not null,
     depart_dt   timestamp,
     arrive_dt   timestamp
-};
-create table used_by {
+);
+create table used_by (
     vehicle_fk  integer REFERENCES vehicles(vehicle_pk) not null,
     convoy_fk   integer REFERENCES convoys(convoy_pk) not null
-};
-create table asset_on {
+);
+create table asset_on (
     asset_fk    integer REFERENCES assets(asset_pk) not null,
     convoy_fk   integer REFERENCES convoys(convoy_pk) not null,
     load_dt timestamp,
     unload_dt   timestamp
-};
-create table users {
+);
+create table users (
     user_pk         serial primary key,
     username        varchar(64),
     active          boolean default FALSE 
-};
-create table roles {
+);
+create table roles (
     role_pk         serial primary key,
     title           varchar(32)
-};
-create table user_is {
+);
+create table user_is (
     user_fk         integer REFERENCES users(user_pk) not null,
-    role_fk         integer REFERENCES roles(role_fk) not null
-}
-create table user_supports {
+    role_fk         integer REFERENCES roles(role_pk) not null
+);
+create table user_supports (
     user_fk         integer REFERENCES users(user_pk) not null,
     facility_fk     integer REFERENCES facilities(facility_pk) not null
-};
-create table levels {
+);
+create table levels (
     level_pk        serial primary key,
     abbrv           varchar(3),
     comment         varchar(128)
-};
-create table compartments {
+);
+create table compartments (
     compartment_pk  serial primary key,
     abbrv           varchar(8),
     comment         varchar(128)
-};
-create table security_tags {
+);
+create table security_tags (
     tag_pk          serial primary key,
     level_fk        integer REFERENCES levels(level_pk) not null,
     compartment_fk  integer REFERENCES compartments(compartment_pk) not null,
     user_fk         integer REFERENCES users(user_pk) not null,
     asset_fk        integer REFERENCES assets(asset_pk),
     product_fk      integer REFERENCES products(product_pk) not null
-};
-
-
-
+);
