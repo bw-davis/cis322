77bd6a88e7b026c0eafc67d4d759498dce886245
Brian Davis <bwdavis11@gmail.com>
2017-02-12 19:30:38 -0800

    app.py is up to date for assignment5

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+import json
 app = Flask(__name__)
  
 @app.route("/")
@@ -10,14 +11,44 @@ def hello():
     return render_template('reportQuery.html')
 
 @app.route("/rest")
-def suspend_user():
+def rest():
 	return render_template('rest.html')
 
-@app.route("/rest/suspend_user", methods=('POST',))
+@app.route("/rest/lost_key", methods=('POST',))
+def rest_key():
+    if request.method=='POST' and 'arguments' in request.form:
+        data=request.form['arguments']
+    return returnJson(theRequest)
+
+@app.route("/rest/activate_user", methods=('POST',))
+def activate_user():
+    if request.method=='POST' and 'arguments' in request.form:
+        data=request.form['arguments']
+    return returnJson(theRequest)
+
+@app.route('/rest/suspend_user', methods=('POST',))
 def suspend_user():
-	if request.method=='POST' and 'arguments' in request.form:
-		data=request.form['arguments']
-	return render_template('data.html', data=data)
+    if request.method=='POST' and 'arguments' in request.form:
+        theRequest=json.loads(request.form['arguments'])
+    return returnJson(theRequest)
+
+@app.route("/rest/list_products", methods=('POST',))
+def list_products():
+    if request.method=='POST' and 'arguments' in request.form:
+        data=request.form['arguments']
+    return returnJson(theRequest)
+
+@app.route("/rest/add_products", methods=('POST',))
+def add_products():
+    if request.method=='POST' and 'arguments' in request.form:
+        data=request.form['arguments']
+    return returnJson(theRequest)
+
+@app.route("/rest/add_asset", methods=('POST',))
+def add_asset():
+    if request.method=='POST' and 'arguments' in request.form:
+        data=request.form['arguments']
+    return returnJson(theRequest)
 
 @app.route("/facility_inventory_report")
 def getInventoryReports():
@@ -30,6 +61,13 @@ def getInTransitReports():				#can put variables in these functions
 @app.route("/logout")
 def logout():
     return render_template('logout.html')
+
+def returnJson(req):
+    theData = dict()
+    theData['timestamp'] = req['timestamp']
+    theData['result'] = 'OK'
+    returnJson = json.dumps(theData)
+    return theData
  
 if __name__ == "__main__":
     app.run()
