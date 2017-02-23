641d8f800795e9013753f68963cd1442ee1084f2
Brian Davis <bwdavis11@gmail.com>
2017-02-23 11:00:05 -0800

    completed steps 1-3

--- preflight.sh
+++ preflight.sh
@@ -1,2 +1,3 @@
+cp src/* $HOME/wsgi
 cd sql
 psql $1 -f create_tables.sql
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,5 +4,31 @@ create table users (
     -- decided to have the username be the primary key to cut down on unnecessary fields
     user_pk         varchar(16) primary key,
     -- wanted to allow for a large password if need be to make user feel comfortable
-    password        varchar(32)
+    password        varchar(32),
+    role_fk 		integer REFERENCES roles(role_pk) 
+);
+
+create table roles (
+	role_pk			serial primary key,
+	role 			varchar(32)
+);
+
+create table assets (
+	asset_pk		serial primary key,
+	asset_tag		varchar(16),
+	description		text,
+	disposed		boolean default FALSE
+);
+
+create table facilities (
+	facility_pk		serial primary key,
+	name			varchar(32),
+	code			varchar(6)
+);
+
+create table asset_at (
+    asset_fk    integer REFERENCES assets(asset_pk) not null,
+    facility_fk integer REFERENCES facilities(facility_pk) not null,
+    arrive_dt   timestamp,
+    depart_dt   timestamp
 );
