1f5d63d6d7cee8b9c06492b90573209212c519f7
Brian Davis <bwdavis11@gmail.com>
2017-02-23 17:25:23 -0800

    made a minor change to create tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,16 +1,16 @@
 -- Created the one table for users, when the scope of the project expands I will adjust accordingly
 
+create table roles (
+	role_pk			serial primary key,
+	role 			varchar(32)
+);
+
 create table users (
     -- decided to have the username be the primary key to cut down on unnecessary fields
     user_pk         varchar(16) primary key,
     -- wanted to allow for a large password if need be to make user feel comfortable
     password        varchar(32),
     role_fk 		integer REFERENCES roles(role_pk) 
-);
-
-create table roles (
-	role_pk			serial primary key,
-	role 			varchar(32)
 );
 
 create table assets (
