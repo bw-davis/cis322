174a65d4e5db088c0b0427a458ffb0d09bb07e65
Brian Davis <bwdavis11@gmail.com>
2017-03-18 21:04:30 -0700

    pushing to test activate_user.py

--- src/app.py
+++ src/app.py
@@ -34,6 +34,37 @@ def create_user():
         return render_template('dashboard.html', username=username)
     return render_template('login.html', error=error)
 
+def activate_user():
+    error = None
+    # if request.method=='GET':
+    #     return render_template('create_user.html')
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        session['username'] = username
+        role = request.form['role']
+        conn =  psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
+        cur = conn.cursor()
+        cur.execute("select count(*) from users where user_pk=%s;", (username, ))
+        count = cur.fetchone()[0]
+        if count == 1:
+            cur.execute("update users set password=%s, active='TRUE' where user_pk=%s;", (password, username, ))
+            return "user's password was updated and their account is now active"
+        if role == 'facofc':
+            cur.execute("insert into users values (%s, %s, 2);", (username, password))
+            session['role'] = 'Facilities Officer'
+        elif role == 'logofc':
+            cur.execute("insert into users values (%s, %s, 1);", (username, password))
+            session['role'] = 'Logistics Officer'
+        else:
+            error = "Role was neither 'facofc' nor 'logofc'"
+            return error
+        conn.commit()
+        cur.close()
+        conn.close()
+        return render_template('dashboard.html', username=username)
+    return render_template('login.html', error=error)
+
 @app.route("/")
 @app.route("/login", methods=('GET', 'POST'))
 def login():
